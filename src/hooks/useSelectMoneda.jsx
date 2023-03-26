a32d81a02fc157b353862c0685a764ba4c242a94
martaalcazarr <martaalcazarredondo@gmail.com>
2023-03-26 00:51:43 -0300

    first commit

---
+++ src/hooks/useSelectMoneda.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const useSelectMoneda = () => {
+ 
+    const SelectMonedas = () => {
+        console.log('desde selectmoneda')
+    }
+
+    return [SelectMonedas]
+}
+
+export default useSelectMoneda
